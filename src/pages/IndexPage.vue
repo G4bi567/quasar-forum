1d7ed9e0672cce577005e4a378b36a4d8f09d81a
G4bi567 <107567915+G4bi567@users.noreply.github.com>
2022-10-29 03:56:09 +0200

    1 file modified

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,11 +5,45 @@
     class="column justify-center align-center"
   >
     <q-timeline-entry heading> Récents </q-timeline-entry>
-    <Post />
+    <Post v-for="link in essentialLinks" :key="link.title" v-bind="link" />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import Post from 'components/PostLayer.vue';
+
+import { defineComponent, ref } from 'vue';
+
+const post = [
+  {
+    problem_title: 'Docs',
+    description_problem: 'quasar.dev',
+    date: 'school',
+    pp_profil: 'https://quasar.dev',
+  },
+  {
+    problem_title: 'Docs',
+    description_problem: 'quasar.dev',
+    date: 'school',
+    pp_profil: 'https://quasar.dev',
+  },
+  {
+    problem_title: 'Docs',
+    description_problem: 'quasar.dev',
+    date: 'school',
+    pp_profil: 'https://quasar.dev',
+  },
+  {
+    problem_title: 'Docs',
+    description_problem: 'quasar.dev',
+    date: 'school',
+    pp_profil: 'https://quasar.dev',
+  },
+  {
+    problem_title: 'Docs',
+    description_problem: 'quasar.dev',
+    date: 'school',
+    pp_profil: 'https://quasar.dev',
+  },
+];
 </script>
