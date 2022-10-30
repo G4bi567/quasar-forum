f738f4d0c367c02cf8c0473f3667104201190f2e
G4bi567 <107567915+G4bi567@users.noreply.github.com>
2022-10-30 11:13:33 +0100

    1 file modified, 1 added, 1 deleted

---
+++ src/components/NewPostLayer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <q-timeline-entry
+      style="
+        margin: 0px 0px 10px 0px;
+        background-color: black;
+        border-radius: 20px;
+        padding: 5px 10px;
+      "
+      color="grey"
+      :title="problem_title"
+      :subtitle="date"
+      :avatar="pp_url"
+    >
+      <div>
+        {{ description_problem }}
+      </div>
+    </q-timeline-entry>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PostLayer',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: String,
+      default: '',
+    },
+
+    description_problem: {
+      type: String,
+      default: '',
+    },
+
+    pp_profil: {
+      type: String,
+      default: '#',
+    },
+
+    pp_exemple: {
+      type: String,
+      default: '#',
+    },
+
+    pp_exemple_own: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -60,9 +60,9 @@ const postList = [
   {
     problem_title: 'probleme de maths',
     date: '29 octobre 2022',
-    description_problem: 'Bonjour, je comprendss pas ca',
     pp_profil:
       'https://th.bing.com/th/id/R.5a537e0b861eb2177ae056f26c87c097?rik=OaqK6FPDWti%2f4g&riu=http%3a%2f%2fwww.magicalmaths.org%2fwp-content%2fuploads%2f2012%2f11%2fmaths_image.jpg&ehk=Ef%2fTZg08YV4z5c1PxaQy1rYcsmjGbiEAxH3L6ifAk3o%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    description_problem: 'Bonjour, je comprendss pas ca',
   },
   {
     problem_title: 'probleme de maths',
